eafa48f1010c6d8ca59907748fead7d31811c3c1
Fraccy <fractalysisofficial@gmail.com>
2023-10-27 00:24:30 -0600

    Add feedback

--- src/vdj_smooth_echo.rs
+++ src/vdj_smooth_echo.rs
@@ -3,7 +3,7 @@ extern crate serde;
 
 use serde::{Deserialize, Serialize};
 use baseplug::{Plugin, ProcessContext};
-use fundsp::{delay, audionode::Frame, prelude::AudioNode, signal::new_signal_frame};
+use fundsp::{delay, audionode::Frame, prelude::AudioNode};
 use typenum::U1;
 
 
@@ -13,7 +13,7 @@ baseplug::model! {
     struct EchoModel {
         #[model(min = 0.001, max = 2.0)]
         #[parameter(name = "time", unit = "Generic",
-            gradient = "Power(1.0)")]
+            gradient = "Power(2.0)")]
         time: f32,
 
         #[model(min = 0.0, max = 1.0)]
@@ -21,7 +21,7 @@ baseplug::model! {
         mix: f32,
 
         #[model(min = 0.0, max = 1.0)]
-        #[parameter(name = "time", unit = "Percentage")]
+        #[parameter(name = "feedback", unit = "Percentage")]
         feedback: f32,
     }
 }
@@ -39,7 +39,10 @@ impl Default for EchoModel {
 
 struct EchoPlug {
     delay_l: delay::Tap<U1,f32>,
-    delay_r: delay::Tap<U1,f32>
+    delay_r: delay::Tap<U1,f32>,
+
+    previous_delay_sample_l: f32,
+    previous_delay_sample_r: f32
 }
 
 impl Plugin for EchoPlug {
@@ -56,7 +59,10 @@ impl Plugin for EchoPlug {
     fn new(_sample_rate: f32, _model: &EchoModel) -> Self {
         let mut ret = EchoPlug {
             delay_l: delay::Tap::new(0.001, 2.0),
-            delay_r: delay::Tap::new(0.001, 2.0)
+            delay_r: delay::Tap::new(0.001, 2.0),
+
+            previous_delay_sample_l: 0.,
+            previous_delay_sample_r: 0.
         };
 
         ret.delay_l.set_sample_rate(_sample_rate as f64);
@@ -71,15 +77,15 @@ impl Plugin for EchoPlug {
         let output = &mut ctx.outputs[0].buffers;
 
         for i in 0..ctx.nframes {
-            let delayed_frame_l = self.delay_l.tick(&Frame::from([
-                input[0][i], model.time[i]
-            ]));
-            let delayed_frame_r = self.delay_r.tick(&Frame::from([
-                input[1][i], model.time[i]
-            ]));
+            self.previous_delay_sample_l = self.delay_l.tick(&Frame::from([
+                input[0][i] + self.previous_delay_sample_l * model.feedback[i], model.time[i]
+            ]))[0];
+            self.previous_delay_sample_r = self.delay_r.tick(&Frame::from([
+                input[1][i] + self.previous_delay_sample_r * model.feedback[i], model.time[i]
+            ]))[0];
 
-            output[0][i] = input[0][i] * (1. - model.mix[i]) + delayed_frame_l[0] * model.mix[i];
-            output[1][i] = input[1][i] * (1. - model.mix[i]) + delayed_frame_r[0] * model.mix[i];
+            output[0][i] = input[0][i] * (1. - model.mix[i]) + self.previous_delay_sample_l * model.mix[i];
+            output[1][i] = input[1][i] * (1. - model.mix[i]) + self.previous_delay_sample_r * model.mix[i];
         }
         
     }
